c2b3aefe75de0c245ac9e9f23432020fe9059b00
Neil Vallon <neilvallon@gmail.com>
2013-11-08 07:42:37 -0600

    Revert to not tracking streams

--- js/matrix.js
+++ js/matrix.js
@@ -7,52 +7,25 @@ Array.prototype.remove = function(from, to) {
 /////////
 
 function Character(x, y, fontSize){
-	this.x = x;
-	this.y = y;
-	this.fontSize = fontSize;
-	this.charStr =  String.fromCharCode(0x30A1 + Math.random() * 89);	
+	this.x = x? x : Math.random() * canvas.width;
+	this.y = y? y : Math.random() * canvas.height;
+	this.fontSize = fontSize? fontSize : Math.random() * (30 - 5) + 5;
+	this.charStr =  String.fromCharCode(0x30A1 + Math.random() * 89);
 }
 
-Character.prototype.draw = function(brightness){
-	var shade = 255 - ((255 - brightness) * 2);
-	
-	cvs.fillStyle = 'rgb(' + shade + ',' + brightness + ',' + shade + ')';
-	cvs.font = this.fontSize + "px Arial";
-	cvs.fillText(this.charStr, this.x, this.y);
-};
-
-
-function Stream(){	
-	this.x = Math.random() * canvas.width;
-	this.y = Math.random() * canvas.height;
-	this.fontSize = Math.random() * (48 - 5) + 5;
-	
-	this.stream = [];
+Character.prototype.isDead = function(){
+	return this.y > canvas.height;
 }
 
-Stream.prototype.nextChar = function(){
-	this.y += this.fontSize;
+Character.prototype.nextCharacter = function(){
+	this.draw(0);
 	return new Character(this.x, this.y + this.fontSize, this.fontSize);
 };
 
-Stream.prototype.isDead = function(){
-	return this.stream.length === 0;
-}
-
-Stream.prototype.draw = function(){
-	if(this.y < canvas.height){
-		var c = this.nextChar();
-		this.stream.unshift(c);
-	}
-	
-	var brightness = 255;
-	for(var i=0; i<this.stream.length; i++){
-		this.stream[i].draw(brightness);
-		if(brightness <= 0 || this.stream[i].y >= canvas.height)
-			this.stream.pop();
-		
-		brightness -= 30;
-	}
+Character.prototype.draw = function(brightness){	
+	cvs.fillStyle = 'rgb(' + brightness + ',' + 255 + ',' + brightness + ')';
+	cvs.font = this.fontSize + "px Arial";
+	cvs.fillText(this.charStr, this.x, this.y);
 };
 
 
@@ -62,25 +35,26 @@ window.onload = function(){
 	canvas.height = window.screen.height;
 	cvs = canvas.getContext("2d");
 	
-	var clearArtifacts = function(){
-		cvs.clearRect(0, 0, canvas.width, canvas.height);
+	var fadeCanvas = function(){
+		cvs.fillStyle = 'rgba(0, 0, 0, 0.25)';
+		cvs.fillRect(0, 0, canvas.width, canvas.height);
 	};
 	
 	var streams = [];
-	
 	var drawMatrix = function(){
+		fadeCanvas();
 		if(streams.length < 50){
-			var s = new Stream;
-			s.draw();
+			var s = new Character;
+			s.draw(255);
 			streams.push(s);
 		}
 		
-		clearArtifacts();
 		for(var i=0; i<streams.length; i++){
 			if(streams[i].isDead()){
 				streams.remove(i);
 			}else{
-				streams[i].draw();
+				streams[i] = streams[i].nextCharacter();
+				streams[i].draw(255);
 			}
 		}
 	};
